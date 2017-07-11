d63cb67db5b037fc35edbc47247749de871c5a2f
Tim Jones <tim@timjones.id.au>
2017-07-11 14:35:48 +1000

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,50 @@
+<?PHP
+    /*
+    MIT License
+
+    Copyright (c) 2017 Tim Jones
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    */
+    
+    $url_slack = '';
+    $url_slack_dev = '';
+
+    $script_name = '';
+    $script_name_dev = '';
+
+    $token_symbol = '';
+    $token_name = '';
+
+    $fiat_1 = '';
+    $fiat_2 = '';
+    $fiat_3 = '';
+
+    $icon_up = '';
+    $icon_flat = '';
+    $icon_down = '';
+    
+    $emoji_up = '';
+    $emoji_flat = '';
+    $emoji_down = '';
+    
+    $colour_up = '';
+    $colour_flat = '';
+    $colour_down = '';
+?>
